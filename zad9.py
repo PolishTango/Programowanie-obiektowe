d835a7947d0b4462763aa13c576864d309f1f3dc
Daniel Fronczak <Daniel Fronczak>
2022-06-03 19:32:04 +0200

    zrobione zmiany

--- zad9.py
+++ zad9.py
@@ -3,5 +3,5 @@ def Zakupy(**kwargs):
     for key, item in kwargs.items():
         print("Przedmiot |", key, "| Ilosc:", item)
         suma+=item
-    print("Laczna ilosc kupionych przedmiotow",suma)
+    print("Laczna ilosc kupionych przedmiotow super zmiana",suma)
 Zakupy(maslo=1, cola=2, popcorn=3)
